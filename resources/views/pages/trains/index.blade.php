5ed28eb78573bdd62ab3bfaf70757a10ac727706
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-25 16:07:56 +0200

    printed the table rows on the page

--- resources/views/pages/trains/index.blade.php
+++ resources/views/pages/trains/index.blade.php
@@ -3,4 +3,23 @@
 @section("page-title", "Trains")
 
 @section("main-content")
+<div class="container">
+    <div class="row justify-content-center">
+        @foreach($trains as $train)
+        <div class="col-4 mb-3">
+            <div class="card">
+                <div class="card-body">
+                    <h3 class="card-title">{{ $train->azienda }}</h3>
+                    <p class="card-text">Partenza: {{ $train->stazione_di_partenza }} - {{ $train->orario_di_partenza }}</p>
+                    <p class="card-text">Arrivo: {{ $train->stazione_di_arrivo }} - {{ $train->orario_di_arrivo }}</p>
+                    <p class="card-text">Codice treno: {{ $train->codice_treno }}</p>
+                    <p class="card-text">Numero carrozze: {{ $train->numero_carrozze }}</p>
+                    <p class="card-text">In orario: {{ $train->in_orario ? 'Sì' : 'No' }}</p>
+                    <p class="card-text">Cancellato: {{ $train->cancellato ? 'Sì' : 'No' }}</p>
+                </div>
+            </div>
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection
