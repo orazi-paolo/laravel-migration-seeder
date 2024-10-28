1c8d5df33ab138e8e6dbc987605e5e484009d03f
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-28 16:22:16 +0100

    populated the seeder and filled the table with random data

--- database/seeders/TrainSeeder.php
+++ database/seeders/TrainSeeder.php
@@ -4,14 +4,28 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+use App\Models\Train;
 
 class TrainSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i=0; $i < 12; $i++) {
+            $newTrain = new Train();
+            $newTrain->azienda = $faker->company();
+            $newTrain->stazione_di_partenza = $faker->city();
+            $newTrain->stazione_di_arrivo = $faker->city();
+            $newTrain->orario_di_partenza = $faker->dateTime();
+            $newTrain->orario_di_arrivo = $faker->dateTime();
+            $newTrain->codice_treno = $faker->bothify('???###');
+            $newTrain->numero_carrozze = $faker->numberBetween(1, 12);
+            $newTrain->in_orario = $faker->boolean();
+            $newTrain->cancellato = $faker->boolean();
+            $newTrain->save();
+        }
     }
 }
