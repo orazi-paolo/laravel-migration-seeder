1cda57e754602d4b7a72ede6aa91fddd9ff717c7
orazi-paolo <orazi.paolo10@gmail.com>
2024-10-28 12:16:52 +0100

    change train controller for view all trains, create passenger controller, create model passenger, create new seeder

--- app/Http/Controllers/TrainController.php
+++ app/Http/Controllers/TrainController.php
@@ -12,10 +12,7 @@ class TrainController extends Controller
 {
     public function index()
     {
-        // carbon now per ottenere la data e l'ora corrente
-        $now = Carbon::now();
-        // seleziono i treni che hanno l'orario di partenza maggiore dell'ora e la data corrente
-        $trains = Train::where('orario_di_partenza', '>=', $now)->get();
+        $trains = Train::all();
         return view('pages.trains.index', compact('trains'));
     }
 }
